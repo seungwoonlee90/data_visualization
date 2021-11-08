bbf2ac40551c5314add4571986e9e01efd53c95f
ethanlee <seungwoonlee90@gmail.com>
2021-11-08 21:41:28 +0900

    dash : more about html components

---
+++ app.py
@@ -0,0 +1,54 @@
+import dash
+from dash import dcc
+from dash import html
+import plotly.express as px
+import pandas as pd
+
+app = dash.Dash(__name__)
+
+colors = {
+    'background': '#02343F',
+    'text': '#F0EDCC'
+}
+
+# assume you have a "long-form" data frame
+# see https://plotly.com/python/px-arguments/ for more options
+df = pd.DataFrame({
+    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
+    "Amount": [4, 1, 2, 2, 4, 5],
+    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
+})
+
+fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
+
+fig.update_layout(
+    plot_bgcolor=colors['background'],
+    paper_bgcolor=colors['background'],
+    font_color=colors['text']
+)
+
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='Hello Dash',
+        style={
+            'textAlign': 'center',
+            'color': colors['text'],
+            'fontSize': 60,
+            'margin': 0,
+            'paddingTop':50
+        }
+    ),
+
+    html.Div(children='Dash: A web application framework for your data.', style={
+        'textAlign': 'center',
+        'color': colors['text']
+    }),
+
+    dcc.Graph(
+        id='example-graph-2',
+        figure=fig
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
