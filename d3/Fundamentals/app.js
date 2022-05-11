ea1fa607f6117f9ed75e334ef10479bf64b17aa4
ethanlee <seungwoonlee90@gmail.com>
2022-05-11 09:47:39 +0900

    enter function

--- d3/Fundamentals/app.js
+++ d3/Fundamentals/app.js
@@ -1,7 +1,13 @@
-const el = d3.select("body")
-    .append("p")
-    .classed("bar", true)
-    .text("hello world!")
-    .style("color", "blue")
+const data = [10, 20, 30, 40, 50]
+const el = d3.select("ul")
+    .selectAll("li")
+    .data(data)
+    .join(
+        enter => {
+            return enter.append("li")
+                .style("color", "purple")
+        }
+    )
+    .text(d => d);
 
 console.log(el)
